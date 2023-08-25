114d2722433cebf84c5fe27fef33562ec470fbbd
RanielSmith <143230725+RanielSmith@users.noreply.github.com>
2023-08-25 20:21:22 -0300

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,44 @@
+print('oi, tudo bem?')
+print('estou com fome!')
+print('uma imagem de cachorro...')
+print('0_____')
+print(' ||||')
+print('''a imagem de um cachorro
+0____
+|||||''')
+print(' meu animal favorito e uma ovelha')
+print('''0-###-
+  | | #''')
+print('moro em são paulo')
+print('''
+         _|_
+        |   |
+        |#  |
+        |   |___
+        |  #|   |
+        |   |  #|
+        |   |  #| ''')
+print('olá'*5)
+print('olá' + 'tudo bem')
+print("ha"*4)
+print("ba"+"na"*2)
+print("Bra"+"sil"+"!"*10)
+print("#"*30)
+print("#"*30)
+print("#"*30)
+print("/\  "*10 )
+print("  \/"*10)
+print('''             __
+            / _)
+    .~^^^~./ /
+ __/        /
+<__.|_|-|_|  ''')
+print('.'*10)
+print('i'*10)
+print('#'*10)
+print('='*10)
+print('#'*10)
+print('='*10)
+print('#'*10)
+
+
